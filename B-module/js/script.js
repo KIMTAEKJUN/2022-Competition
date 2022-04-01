a4c0774685db5c9e4a7bc772272a1830455cf208
KimTaekJun <gdsygrt557422@naver.com>
2022-04-01 19:56:47 +0900

    .

---
+++ B-module/js/script.js
@@ -0,0 +1,13 @@
+// let gamestatus = false;
+// let count = 0;
+// let timeout = false;
+// let stack = 0;
+// let i = 0;
+$(()=> {
+    const main = {
+        main : () => {
+            console.log(1);
+        }
+    }
+    main.main();
+});
